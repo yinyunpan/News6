b28373319b9464cb94bffe68c2d1af772e97e5ba
攀攀 <308635215@qq.com>
2015-01-25 17:27:47 +0800

    bbbb

--- src/com/aoqi3d/news6/MainActivity.java
+++ src/com/aoqi3d/news6/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);//bb
 		setContentView(R.layout.activity_main);
 	}
 
