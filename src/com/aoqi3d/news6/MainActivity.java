222f198f7de91a04462a24502e3f048560b34b1f
攀攀 <308635215@qq.com>
2015-01-25 18:00:36 +0800

    12345678

--- src/com/aoqi3d/news6/MainActivity.java
+++ src/com/aoqi3d/news6/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);//bb
 		setContentView(R.layout.activity_main);//nnnnnn
+		//12345678
 	}
 
 	@Override
